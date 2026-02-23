6e9bed1581c95d001f5723cdde9de0d94e7d523d
Aditya Soni <adia12528@gmail.com>
2026-02-23 17:42:40 +0530

    initial commit all updates done

--- run-all.bat
+++ run-all.bat
@@ -1,2 +1,4 @@
 start cmd /k "cd backend && npm install && npm start"
 start cmd /k "cd frontend && npm install && npm start"
+
+@REM // stating commands for backend and frontend in separate command prompt windows. Each command prompt will navigate to the respective directory, install necessary dependencies, and start the application.
