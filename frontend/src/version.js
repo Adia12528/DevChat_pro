a5eb6fe89ac44def2223901323b967a454d41da4
Aditya Soni <adia12528@gmail.com>
2026-02-27 18:17:18 +0530

    UI/UX new new version 2.11.0

--- frontend/src/version.js
+++ frontend/src/version.js
@@ -1,5 +1,5 @@
 // Auto-generated version file - DO NOT EDIT MANUALLY
 // This file is updated automatically during build
-export const APP_VERSION = '2.10.8';
-export const BUILD_DATE = '2026-02-26T18:32:46.951Z';
+export const APP_VERSION = '2.2.0';
+export const BUILD_DATE = '2026-02-27';
 export const CACHE_VERSION = `devchat-pro-v${APP_VERSION}`;
