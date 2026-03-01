e77cf26e921a64bf749e5860e01ee79e7de83373
Aditya Soni <adia12528@gmail.com>
2026-03-01 19:12:50 +0530

    Fix rename

--- frontend/src/components/settings/SettingsManager.js
+++ frontend/src/components/settings/SettingsManager.js
@@ -5,7 +5,7 @@ import AudioSettings from './AudioSettings';      // Capital A, Capital S
 import VideoSettings from './VideoSettings';      // Capital V, Capital S
 import StreamSettings from './StreamSettings';    // Capital S, Capital S
 import AppSettings from './AppSettings';          // Capital A, Capital S
-import CallHistoryPanel from '../../CallHistoryPanel'; // Capital C, Capital H, Capital P
+import CallHistoryPanel from '../CallHistoryPanel'; // Capital C, Capital H, Capital P
 
 const SettingsManager = ({
   currentView,
