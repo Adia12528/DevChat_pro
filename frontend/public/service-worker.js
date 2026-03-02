72cf7e4755ad6d8944fa0708ff395fd3196ff231
Aditya Soni <adia12528@gmail.com>
2026-03-02 11:26:59 +0530

    feature fix

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-restricted-globals */
 
-const CACHE_NAME = 'devchat-pro-v2.2.0-b1772306358350';
+const CACHE_NAME = 'devchat-pro-v2.2.0-b1772429808388';
 const urlsToCache = [
   '/',
   '/static/css/main.css',
