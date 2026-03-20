13097ba7993055cadd96a04342c45b56a9ea1906
Aditya Soni <adia12528@gmail.com>
2026-03-20 23:10:23 +0530

    Updated Application for devroom

--- frontend/src/version.js
+++ frontend/src/version.js
@@ -1,5 +1,5 @@
 // Auto-generated version file - DO NOT EDIT MANUALLY
 // This file is updated automatically during build
 export const APP_VERSION = '2.2.2';
-export const BUILD_DATE = '2026-03-20T07:26:27.018Z';
+export const BUILD_DATE = '2026-03-20T17:39:43.338Z';
 export const CACHE_VERSION = `devchat-pro-v${APP_VERSION}`;
